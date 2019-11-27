6e49439ef90e2517940c1d7910cfb209c7d2e472
Arjun Mittal <arm197@pitt.edu>
2019-11-27 11:23:10 -0500

    This is my final seal_test data file... I hope

--- seal_test.R
+++ seal_test.R
@@ -3,7 +3,8 @@ sealData <- read.csv("sealData.csv")
 head(sealData) #peak at data set
 #This is to create a column with the difference in 02 consumption between dives
 sealData$difference <- sealData$O2.after.feeding.dive - sealData$O2.after.nonfeeding.dive
-sealData
-t.test(sealData$difference)
+sealData #view new differences
+resultsOfTest <- t.test(sealData$difference)
+cat(resultsOfTest,file = "T-Test Results")
 #cat command to create file
 
